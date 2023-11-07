e8e0b5091034ec697beedeea7827ad7e303fc179
AliSiaa <alicia.roman.169@eaceuta.com>
2023-11-07 14:20:31 +0100

    test enlaces

--- Portada.js
+++ Portada.js
@@ -1,4 +1,4 @@
 const derecha = document.querySelector(".positionFlecha");
 derecha.addEventListener("click", function(){
-    window.location.href = "../paginas/1.html";
+    window.location.href = "paginas/1.html";
 })
